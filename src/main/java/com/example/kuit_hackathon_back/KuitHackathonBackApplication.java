7eac8c119f78e31369f331cd414e884f36de297a
ryjun29 <happy042957@gmail.com>
2026-07-01 22:36:52 +0900

    Chore: Initialize Spring Boot Project

---
+++ src/main/java/com/example/kuit_hackathon_back/KuitHackathonBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.kuit_hackathon_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuitHackathonBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuitHackathonBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kuit_hackathon_back/KuitHackathonBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kuit_hackathon_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuitHackathonBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
