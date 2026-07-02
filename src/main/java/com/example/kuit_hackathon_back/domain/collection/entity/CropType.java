2d23cc18127b78d22f393438a1105dcd584e8b3f
ryjun29 <happy042957@gmail.com>
2026-07-02 21:44:59 +0900

    Fix: crop_type enum 값 및 companion_type DB comment 불일치 수정

--- src/main/java/com/example/kuit_hackathon_back/domain/collection/entity/CropType.java
+++ src/main/java/com/example/kuit_hackathon_back/domain/collection/entity/CropType.java
@@ -3,6 +3,7 @@ package com.example.kuit_hackathon_back.domain.collection.entity;
 /** 곤충 SVG 마스크 종류. */
 public enum CropType {
     BUTTERFLY,
-    BEETLE,
-    DRAGONFLY
+    MOTH,
+    SNAIL,
+    BEETLE
 }
