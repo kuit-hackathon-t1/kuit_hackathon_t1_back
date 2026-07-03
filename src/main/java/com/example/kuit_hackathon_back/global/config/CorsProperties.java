123ffe9ef199b28d32f51ece1d09c94d1ac451e2
ryjun29 <happy042957@gmail.com>
2026-07-03 14:54:31 +0900

    "FIX: spotless 포맷 위반 수정"

--- src/main/java/com/example/kuit_hackathon_back/global/config/CorsProperties.java
+++ src/main/java/com/example/kuit_hackathon_back/global/config/CorsProperties.java
@@ -16,8 +16,8 @@ import lombok.Setter;
  *   allowed-origins: ${CORS_ALLOWED_ORIGINS:http://localhost:3000,http://localhost:5173}
  * </pre>
  *
- * <p>콤마로 구분된 문자열을 Spring이 자동으로 List&lt;String&gt;으로 바인딩해준다. 배포 환경에서 프론트
- * 도메인이 추가/변경되면 코드 수정 없이 Render 환경변수(CORS_ALLOWED_ORIGINS)만 바꾸면 된다.
+ * <p>콤마로 구분된 문자열을 Spring이 자동으로 List&lt;String&gt;으로 바인딩해준다. 배포 환경에서 프론트 도메인이 추가/변경되면 코드 수정 없이
+ * Render 환경변수(CORS_ALLOWED_ORIGINS)만 바꾸면 된다.
  */
 @Component
 @Getter
--- src/main/java/com/example/kuit_hackathon_back/global/config/WebConfig.java
+++ src/main/java/com/example/kuit_hackathon_back/global/config/WebConfig.java
@@ -26,9 +26,8 @@ public class WebConfig implements WebMvcConfigurer {
     /**
      * 프론트(Vercel 배포) 및 로컬 개발 환경에서의 API 호출을 허용한다.
      *
-     * <p>허용 origin 목록은 {@link CorsProperties}(cors.allowed-origins 프로퍼티)에서 주입받는다. Vercel은
-     * 브랜치/커밋마다 프리뷰 URL이 새로 생성될 수 있어서, 배포 환경에서는 CORS_ALLOWED_ORIGINS 환경변수로 필요한 도메인을 코드 수정 없이
-     * 추가/변경할 수 있다.
+     * <p>허용 origin 목록은 {@link CorsProperties}(cors.allowed-origins 프로퍼티)에서 주입받는다. Vercel은 브랜치/커밋마다
+     * 프리뷰 URL이 새로 생성될 수 있어서, 배포 환경에서는 CORS_ALLOWED_ORIGINS 환경변수로 필요한 도메인을 코드 수정 없이 추가/변경할 수 있다.
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
